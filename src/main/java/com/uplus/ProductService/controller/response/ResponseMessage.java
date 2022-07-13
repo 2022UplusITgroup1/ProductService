ca248e046178a528ce6a8375932672ff0faed9b9
avocado51 <ujmikzz7@hanmail.net>
2022-07-13 11:01:56 +0900

    Add Response message formats

---
+++ src/main/java/com/uplus/ProductService/controller/response/ResponseMessage.java
@@ -0,0 +1,14 @@
+package com.uplus.ProductService.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ResponseMessage {
+    private Integer status;
+    private String message;
+    private Object data;
+}
