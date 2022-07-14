ff6cbd2ba822e590239614e4c144f66a9bfd8fbe
avocado51 <ujmikzz7@hanmail.net>
2022-07-14 16:59:21 +0900

    Add read plan list operation

---
+++ src/main/java/com/uplus/productservice/repository/PlanRepository.java
@@ -0,0 +1,14 @@
+package com.uplus.productservice.repository;
+
+import com.uplus.productservice.domain.plan.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+////////////////////////////////////
+// Create Date: 2022.07.14        //
+// Create By: MYSEO              //
+///////////////////////////////////
+public interface PlanRepository extends JpaRepository<Plan, Integer> {
+    List<Plan> findAllByNetworkSupportOrderById(String networkSupport);
+}
---
+++ src/main/java/com/uplus/productservice/service/PlanService.java
@@ -0,0 +1,22 @@
+package com.uplus.productservice.service;
+
+import com.uplus.productservice.domain.plan.Plan;
+import com.uplus.productservice.repository.PlanRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+////////////////////////////////////
+// Create Date: 2022.07.14        //
+// Create By: MYSEO              //
+///////////////////////////////////
+@Service
+@RequiredArgsConstructor
+public class PlanService {
+    private final PlanRepository planRepository;
+
+    public List<Plan> getPlanList(String networkSupport) {
+        return planRepository.findAllByNetworkSupportOrderById(networkSupport);
+    }
+}
