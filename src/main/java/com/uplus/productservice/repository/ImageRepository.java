699fd4780b80e234e85ad8c492a8104532ca8fff
avocado51 <ujmikzz7@hanmail.net>
2022-07-15 00:56:50 +0900

    Add phone_image repository

---
+++ src/main/java/com/uplus/productservice/repository/ImageRepository.java
@@ -0,0 +1,14 @@
+package com.uplus.productservice.repository;
+
+import com.uplus.productservice.domain.phone.Images;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+////////////////////////////////////
+// Create Date: 2022.07.14        //
+// Create By: MYSEO              //
+///////////////////////////////////
+public interface ImageRepository extends JpaRepository<Images, Integer> {
+    List<Images> findByPhoneId(Integer phoneId);
+}
